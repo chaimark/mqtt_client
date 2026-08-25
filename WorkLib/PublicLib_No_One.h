dc29a7c14bb2fe0e3ce59289331c46dac6e7cd1e
chai21sks <chai21sks@gmail.com>
2026-08-25 23:12:09 +0800

    Tue Aug 25 23:12:09 CST 2026

--- WorkLib/PublicLib_No_One.h
+++ WorkLib/PublicLib_No_One.h
@@ -2,7 +2,6 @@
 #define __PUBLIC_LIB_NO_ONE_H__
 
 // 公共库头文件, 用于包含一些非库使用的公共定义, 或声明一些公共使用的全局结构体
-#include "FreeRTOSConfig.h"
 #include "SetTime.h"
 extern timetask RTC_Task;
 extern timetask Uart_Task;
@@ -43,6 +42,5 @@ typedef struct _TimeStuClass {
     软件定时器优先级 configTIMER_TASK_PRIORITY 32 高于普通线程
     rtos 临界优先级 configMAX_SYSCALL_INTERRUPT_PRIORITY
 */
-
 
 #endif
