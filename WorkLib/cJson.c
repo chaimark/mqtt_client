a5d2c01793b476bd9987fea019bd623925674088
chaimark <chai21sks@gmail.com>
2026-08-31 08:44:02 +0000

    Mon Aug 31 08:44:02 AM UTC 2026

--- WorkLib/cJson.c
+++ WorkLib/cJson.c
@@ -580,8 +580,8 @@ bool checkOfCsJsonStrIsRight(strnew JsonInputStr, strnew JsonOutputStr) {
         copyString(JsonOutputStr.Name._char, JsonInputStr.Name._char, JsonOutputStr.MaxLen, JsonInputStr.MaxLen);
     }
     int DataLen = strlen(JsonInputStr.Name._char);
-    JsonInputStr.Name._char[DataLen++] = (((uint16_t)(NowCheckNum)&0xFF00) >> 8);
-    JsonInputStr.Name._char[DataLen++] = (((uint16_t)(NowCheckNum)&0x00FF) >> 0);
+    JsonInputStr.Name._char[DataLen++] = (((uint16_t)(NowCheckNum) & 0xFF00) >> 8);
+    JsonInputStr.Name._char[DataLen++] = (((uint16_t)(NowCheckNum) & 0x00FF) >> 0);
     // 计算 cs 或其他检验算法
     return is_crc_modbus(JsonInputStr, DataLen);
 }
